3814e90316a4c8bbe11a11c4b36c957d0376065d
Tyler Rutherford <rutherford87@gmail.com>
2021-04-05 09:38:50 -0500

    includes README text. javascript now adds buttons, and day 1 and day 2 forecast.

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # WhatsTheWeather
+
+Contents of repo:
+* index.html
+* Example image
+* README.md
+* script.js
+
+This is a weather dashboard I created called "What's the Weather?". It was created using a Tailwind CDN and Moment.
+
+The API is called to the OpenWeather source URL. 
+
+The repo can be found here: 
+[TPR_GitHub](https://github.com/rutherford87/WhatsTheWeather)
+
+The website can be viewed here: [What's the Weather?](https://rutherford87.github.io/WhatsTheWeather/)
+
+Here is a screenshot of  quiz:
+![WOW!](./ExampleLiveQuiz.PNG)
+
+How to use this tool:
+Search by city in the search box and click submit.
+Each city the user searches for is saved in a list and can be recalled by clicking on the city name. 
+
+Good luck, have fun!
+
+Expansion work:
+* Create a for loop for Day[i], where i goes from day 1 through 5
+* Continue to explore styling from Tailwind
--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 //Selectors
-var todayTemp = document.getElementById("todayTemp");
+var cityHeader = document.getElementById('cityHeader');
+var todayTempEl = document.getElementById("todayTemp");
+var todayWindEl = document.getElementById('todayWind');
+var todayHumidEl = document.getElementById('todayHumid');
+var todayUVEl = document.getElementById('todayUV');
 var submitBtn = document.getElementById('submitBtn');
 var inputCityEl = document.getElementById('inputCity');
 var containerEl = document.getElementsByClassName('container');
 var cityList = [];
+var today = moment();
+
+//Day 1 Forecast selectors
+var day1Temp = document.getElementById('day1Temp');
+var day1Wind = document.getElementById('day1Wind');
+var day1Humid = document.getElementById('day1Humid');
+var day1UV = document.getElementById('day1UV');
+
+//Day 2 Forecast selectors
+var day2Temp = document.getElementById('day2Temp');
+var day2Wind = document.getElementById('day2Wind');
+var day2Humid = document.getElementById('day2Humid');
+var day2UV = document.getElementById('day2UV');
 
 //Functions
   //getWeatToday will fetch the current conditions from the city in the form after clicking submit
@@ -22,12 +39,12 @@ function getWeatToday(event) {
 } 
       for (var i=0; i<cityList.length; i++){
  
-       var relistCity = document.createElement('li');
+       var relistCity = document.createElement('BUTTON');
       var text = document.createTextNode(cityList[i]);
       relistCity.appendChild(text);
       cityUl.appendChild(relistCity);
     }
-  var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' +inputCity+ '&appid=b1c903e5d1f873b35b51140c6c7621da';  
+  var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' +inputCity+ '&units=imperial&appid=b1c903e5d1f873b35b51140c6c7621da';  
  
   fetch(requestUrl)
     .then(function (response) {
@@ -37,30 +54,23 @@ function getWeatToday(event) {
     })
     .then(function (data) {
      console.log(data)
-        todayTemp.textContent=("Temp: " +data.list[0].main.temp);
+        cityHeader.textContent=(inputCity+' on ' +(today.format("dddd, MMM Do, YYYY")));
+        todayTempEl.textContent=("Temp: " +data.list[0].main.temp +'°F');
+        todayWindEl.textContent=('Wind: ' + data.list[0].wind.speed + ' mph');
+        todayHumidEl.textContent=('Humidity: '+data.list[0].main.humidity+' %');
         console.log(data.list[0].dt)
+
+        day1Temp.textContent=('Temp: ' +data.list[8].main.temp);
+        day1Wind.textContent=('Wind: ' + data.list[8].wind.speed + ' mph');
+        day1Humid.textContent=('Humidity: '+data.list[8].main.humidity+' %');
+       
+        day2Temp.textContent=('Temp: ' +data.list[16].main.temp);
+        day2Wind.textContent=('Wind: ' + data.list[16].wind.speed + ' mph');
+        day2Humid.textContent=('Humidity: '+data.list[16].main.humidity+' %');
     });
 }
 //data has 5 day forecast in it - just call the correct [hour] from list when making the 5 day cards...
-console.log('form no function' +inputCity);
 
-function getForecast() {
-    // fetch request gets a list of all the repos for the node.js organization
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=London,uk&appid=b1c903e5d1f873b35b51140c6c7621da';//compare the hours returned to the above to see if it is a forecast
-      
-    fetch(requestUrl)
-      .then(function (response) {
-        // console.log(response);
-        return response.json();
-        //console.log(response)
-      })
-      .then(function (data) {
-       console.log(data)
-        //Loop over the data to generate a table, each table row will have a link to the repo url
-          todayTemp.textContent=("Temp: " +data.list[0].main.temp);
-          console.log(data.list[0].dt)
-      });
-  }
- 
+
 //Event Listeners
 submitBtn.addEventListener("click", getWeatToday); 
